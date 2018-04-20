30eceac66ed8393617fe5b62a3ac8e3fad1b747a
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-04-20 21:55:23 +0530

    Add files via upload

---
+++ Java/hello.java
@@ -0,0 +1,13 @@
+/*
+ * This file gives the hello world message after compiling and running it
+
+ * $ javac hello.java[ENTER]		...Compile, Creates default object file
+ * $ java hello[ENTER]			...Run
+ * $ rm hello.class[ENTER]		...Delete
+ 
+ */
+public class hello{
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+	}
+}
